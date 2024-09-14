7456ff9c2e743636a4aca42aa64eed6358edb225
Pranab Shukla <pranab.austin@gmail.com>
2024-09-14 16:10:43 -0500

    add hover effects

--- Math Calculator/script.js
+++ Math Calculator/script.js
@@ -4,29 +4,33 @@ function $(id) {
 }
 
 const btn = $("enter-btn");
-const inputVal1 = $("numtext1").value;
-const inputVal2 = $("numtext2").value;
-const result = $("result-label");
-const operation = $("operation").value;
+var inputVal1 = $("numtext1").value;
+var inputVal2 = $("numtext2").value;
+var result = $("result-label");
+var operation = $("operation").value;
 console.log(btn, inputVal1, inputVal2, result, operation)
 let resultNum =  0;
 function generateAnswer() {
+    inputVal1 = $("numtext1").value;
+    inputVal2 = $("numtext2").value;
+    operation = $("operation").value;
+    console.log(btn, inputVal1, inputVal2, result, operation)
 
     switch (operation) {
         case "+":
-            resultNum = inputVal1 + inputVal2;
+            resultNum = parseInt(inputVal1) + parseInt(inputVal2);
             break;
         case "-":
-            resultNum = inputVal1 - inputVal2;
+            resultNum = parseInt(inputVal1) - parseInt(inputVal2);
             break;
         case "*":
-            resultNum = inputVal1 * inputVal2;
+            resultNum = parseInt(inputVal1) * parseInt(inputVal2);
             break;
         case "/":
-            resultNum = inputVal1 / inputVal2;
+            resultNum = parseInt(inputVal1) / parseInt(inputVal2);
             break;
         case "%":
-            resultNum = inputVal1 % inputVal2;
+            resultNum = parseInt(inputVal1) % parseInt(inputVal2);
             break;
     }
     result.innerHTML = `Answer: ${resultNum}`;
